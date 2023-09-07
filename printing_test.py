c73bce6845524d959b6488dbe8c0f5aedf8a882b
Corentin Dieval <corentin@graphmytech.fr>
2023-09-07 12:19:17 +0200

    main::CDI:: testing to install a private package

---
+++ printing_test.py
@@ -0,0 +1,5 @@
+def hello_world(text: str) -> str:
+    if text:
+        return f'Hello world, {text}'
+    else:
+        return 'Hello world, welcome to Python test'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Githubaction_test',
+    version='0.0.1',
+    packages=['Githubaction_test'],
+    url='https://github.com/dievalcorentin/Githubaction_test',
+    license='',
+    author='Corentin Dieval',
+    author_email='corentin@graphmytech.fr',
+    description='Testing GitHub Action and creation of package'
+)
