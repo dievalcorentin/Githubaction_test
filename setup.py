374d473f41fd316f12b5bc8e4d71cd50d256b84b
Corentin Dieval <corentin@graphmytech.fr>
2023-09-07 15:54:24 +0200

    main::CDI:: tag v.0.0.3

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Githubaction_test',
-    version='0.0.2',
-    packages=['Githubaction_test'],
+    version='0.0.3',
+    packages=find_packages('', exclude=['test']),
     url='https://github.com/dievalcorentin/Githubaction_test',
     license='',
     author='Corentin Dieval',
