fc7e8153d7d35644c29ac3bbb0262b1b4cfa2c1b
Corentin Dieval <corentin@graphmytech.fr>
2023-09-07 14:15:17 +0200

    main::CDI:: add requirement.txt

--- printing_test.py
+++ printing_test.py
@@ -1,5 +1,4 @@
 def hello_world(text: str) -> str:
     if text:
         return f'Hello world, {text}'
-    else:
-        return 'Hello world, welcome to Python test'
+    return 'Hello world, welcome to Python test'
